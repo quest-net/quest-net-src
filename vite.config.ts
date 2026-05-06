d606fe49e2cae67c04ae0d434234ac12c2a03750
sguimez <sguimez@donorbureau.com>
2026-05-06 12:19:34 -0500

    cleaned up unused packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		tailwindcss(), // Add Tailwind as a Vite plugin
+		tailwindcss(),
 	],
 	base: "/",
 	server: {
 		port: 3000,
 	},
 	build: {
-		outDir: "build", // Keep 'build' folder for compatibility with your deploy script
+		outDir: "build",
 	},
 });
