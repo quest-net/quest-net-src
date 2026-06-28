51ea2e0cadae7fba1bf10dd952a571ae3a8d6e89
Samuel <samy.guimez@gmail.com>
2026-06-28 18:28:44 -0500

    version bump

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // src/version.ts
 export type VersionString = `${number}.${number}.${number}`;
 
-export const APP_VERSION: VersionString = "3.0.0";
+export const APP_VERSION: VersionString = "3.0.1";
